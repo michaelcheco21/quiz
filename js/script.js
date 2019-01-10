d20a46e95bb04925e881edb79b9d0be58b0f32e6
Michael Checo <michaeljcheco@gmail.com>
2019-01-10 23:26:30 +0000

    adding if/else statements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /* global $ */
 
 $(document).ready(function() {
+    
     $("button").click(function() {
         var name = $("#name").val();
         var finalAnswer = "TBD";
@@ -8,7 +9,84 @@ $(document).ready(function() {
         var q1Input = $("#question1").val();
         var q2Input = $("#question2").val();
         var q3Input = $("#question3").val();
-        var totalScore;
+        var q4input = $("#question4").val();
+        
+        var final = finalScore(q1Input, q2Input, q3Input, q4input);
+        
+        if(final >=18){
+            
+        }
+        
+        
+        var totalScore = questionOne(q1Input);
+        totalScore += questionTwo(q2Input);
+        totalScore += questionThree(q3Input);
+        totalScore += questionFour(q4input);
+        
+        
     });
+    
+    function questionOne(answer){
 
+        if(answer === 1){
+            return 2;
+        }else if(answer === 2){
+             return 3;
+        }else if(answer === 3){
+             return 5;
+        }else if(answer === 4){
+             return 4;
+        }else{
+             return 0;
+        }
+    }
+    
+    function questionTwo(answer){
+
+        if(answer === 1){
+            return 5;
+        }else if(answer === 2){
+            return 2;
+        }else if(answer === 3){
+            return 3;
+        }else if(answer === 4){
+            return 4;
+        }else{
+            return 0;
+        }
+    }
+    
+    function questionThree(answer){
+
+        if(answer === 1){
+            return 4;
+        }else if(answer === 2){
+            return 5;
+        }else if(answer === 3){
+            return 2;
+        }else if(answer === 4){
+            return 3;
+        }else{
+            return 0;
+        }
+    }
+    
+    function questionFour(answer){
+
+        if(answer === 1){
+            return 2;
+        }else if(answer === 2){
+            return 3;
+        }else if(answer === 3){
+            return 5;
+        }else if(answer === 4){
+            return 4;
+        }else{
+            return 0;
+        }
+    }
+    
+    function finalScore(one, two, three, four){
+        return one + two + three + four;
+    }
 });
