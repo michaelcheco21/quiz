a790cc5f76bc5ab735332c4ef1c11f555c513a7d
Michael Checo <michaeljcheco@gmail.com>
2019-01-24 23:27:16 +0000

    backup

--- js/script.js
+++ js/script.js
@@ -1,39 +1,45 @@
 /* global $ */
 
 $(document).ready(function() {
-    
+    console.log("document.ready");
     $("button").click(function() {
+        console.log("button.click");
         var name = $("#name").val();
-        var finalAnswer = "TBD";
+        var finalAnswer;
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Input = $("#question1").val();
-        var q2Input = $("#question2").val();
-        var q3Input = $("#question3").val();
-        var q4input = $("#question4").val();
+        var q1Input = parseInt($("#question1").val());
+        var q2Input = parseInt($("#question2").val());
+        var q3Input = parseInt($("#question3").val());
+        var q4Input = parseInt($("#question4").val());
         
-        var final = finalScore(q1Input, q2Input, q3Input, q4input);
-        
-        if(final >=18){
+        var final = calcFinalScore(getScoreOne(q1Input), getScoreTwo(q2Input), getScoreThree(q3Input), getScoreFour(q4Input) );
+
+        if(final === 16){
+            alert(name + " ," + "you are Hades, God of the Underworld");
+            $(".result").prepend("<img src='http://www.gods-and-monsters.com/images/xhades-god-of-underworld.jpg.pagespeed.ic.g_J4O19QSg.jpg'>");
+            
+        }else if(final <= 15 && final >= 12){
+            alert(name + " ," + "you are Zeus, God of Lightning");
+            
+        }else if(final <=11 && final >= 9){
+            alert(name + " ," + "you are Poseidon, God of the Sea");
+            
+        }else if(final <= 8){
+            alert(name + " ," + "you are Aphrodite, Goddess of Love");
             
         }
         
-        
-        var totalScore = questionOne(q1Input);
-        totalScore += questionTwo(q2Input);
-        totalScore += questionThree(q3Input);
-        totalScore += questionFour(q4input);
-        
-        
+
     });
     
-    function questionOne(answer){
+    function getScoreOne(answer){
 
         if(answer === 1){
-            return 2;
+            return 1;
         }else if(answer === 2){
-             return 3;
+             return 2;
         }else if(answer === 3){
-             return 5;
+             return 3;
         }else if(answer === 4){
              return 4;
         }else{
@@ -41,10 +47,10 @@ $(document).ready(function() {
         }
     }
     
-    function questionTwo(answer){
+    function getScoreTwo(answer){
 
         if(answer === 1){
-            return 5;
+            return 1;
         }else if(answer === 2){
             return 2;
         }else if(answer === 3){
@@ -56,29 +62,14 @@ $(document).ready(function() {
         }
     }
     
-    function questionThree(answer){
+    function getScoreThree(answer){
 
         if(answer === 1){
-            return 4;
+            return 1;
         }else if(answer === 2){
-            return 5;
-        }else if(answer === 3){
             return 2;
-        }else if(answer === 4){
-            return 3;
-        }else{
-            return 0;
-        }
-    }
-    
-    function questionFour(answer){
-
-        if(answer === 1){
-            return 2;
-        }else if(answer === 2){
-            return 3;
         }else if(answer === 3){
-            return 5;
+            return 3;
         }else if(answer === 4){
             return 4;
         }else{
@@ -86,7 +77,22 @@ $(document).ready(function() {
         }
     }
     
-    function finalScore(one, two, three, four){
-        return one + two + three + four;
+    function getScoreFour(answer){
+
+        if(answer === 1){
+            return 1;
+        }else if(answer === 2){
+            return 2;
+        }else if(answer === 3){
+            return 3;
+        }else if(answer === 4){
+            return 4;
+        }else{
+            return 0;
+        }
+    }
+    
+    function calcFinalScore(answOne, answTwo, answThree, answFour){
+        return answOne + answTwo + answThree + answFour;
     }
 });
